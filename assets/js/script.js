32a56b86e8d277478c6456b5d5639bf3634d820d
Emily N Mayeski <enmayeski@gmail.com>
2021-06-09 00:31:16 -0400

    added deletion to task items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // ADD TASK TO TASK LIST
+var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
@@ -91,5 +92,27 @@ var createTaskActions = function(taskId) {
 };
 
 formEl.addEventListener("submit", taskFormHandler);
+
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+//DELETE TASK FUNCTION
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
+
+
+
+
+//GENERAL EVENT LISTENER
+pageContentEl.addEventListener("click", taskButtonHandler);
 
 
