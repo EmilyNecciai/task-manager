bd9b11ea731126e60bf3a72906ec1ac587a3c530
Emily N Mayeski <enmayeski@gmail.com>
2021-06-09 23:53:46 -0400

    tasks now move to appropriate columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -143,9 +145,26 @@ var editTask = function(taskId) {
     formEl.setAttribute("data-task-id", taskId);
 };
 
+//CHANGE COLUMN FUNCTION
+var taskStatusChangeHandler = function(event) {
+    var taskId = event.target.getAttribute("data-task-id");
+    var statusValue = event.target.value.toLowerCase();
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+    } else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+    } else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+};
 
 
 //GENERAL EVENT LISTENER
 pageContentEl.addEventListener("click", taskButtonHandler);
+
+//change column event listener
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
 
 
